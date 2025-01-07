be593dc5314f096ba4044f37ddda1f74d72316e8
Kapil Tiwari <tiwarikapil216@gmail.com>
2025-01-07 16:45:18 +0530

    new change

--- NEWAR.java
+++ NEWAR.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class welcome{
 public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
+//hello world
 int x = sc.nextInt();
 int y = sc.nextInt();
 int z = sc.nextInt();
